bbc1c9e3f39f53e623c60086cdf3a8a1bb4c7e1e
AMMANANASATISH <AMMANANASATISH@DESKTOP-GV9D5ET>
2018-02-13 23:43:00 +0530

    Conver Jar to War File to deploy in Tomcat Server

--- src/main/java/guru/springframework/SpringBootWebApplication.java
+++ src/main/java/guru/springframework/SpringBootWebApplication.java
@@ -2,11 +2,29 @@ package guru.springframework;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 
-@SpringBootApplication
+/*@SpringBootApplication
 public class SpringBootWebApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(SpringBootWebApplication.class, args);
     }
+}*/
+
+//Convert Jar to War To Deploy in Tomcat
+@SpringBootApplication
+public class SpringBootWebApplication extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		
+		return super.configure(builder);
+	}
+	
+	 public static void main(String[] args) throws Exception {
+	        SpringApplication.run(SpringBootWebApplication.class, args);
+	    }
+
 }
